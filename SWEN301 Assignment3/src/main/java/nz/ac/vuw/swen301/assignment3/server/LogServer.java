5fb71fc99bdf640fd842d563594478f3098179cf
Riley Grice <gricerile@ecs.vuw.ac.nz>
2019-06-04 15:21:48 +1200

    name changes and set ups have been made so a server can potentially run soon

--- SWEN301 Assignment3/src/main/java/nz/ac/vuw/swen301/assignment3/server/LogServer.java
+++ SWEN301 Assignment3/src/main/java/nz/ac/vuw/swen301/assignment3/server/LogServer.java
@@ -1,4 +1,16 @@
 package nz.ac.vuw.swen301.assignment3.server;
 
-public class LogServer {
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LogServer extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response){
+        int num = Integer.parseInt(request.getParameter("limit"));
+
+    }
+    public void doPost(HttpServletRequest request, HttpServletResponse response){
+
+    }
 }
